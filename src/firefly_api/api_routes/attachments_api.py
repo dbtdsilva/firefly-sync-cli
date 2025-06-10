71ac7ba7413a3de83a29ef073571d9cef8210875
Diogo Silva <dbtdsilva@gmail.com>
2025-06-11 00:14:50 +0200

    fix: bypass is no longer required as it was fixed by firefly

--- src/firefly_api/api_routes/attachments_api.py
+++ src/firefly_api/api_routes/attachments_api.py
@@ -13,20 +13,6 @@ class AttachmentsApi(BaseApi):
         response = self._internal_post('/', attachment.model_dump(exclude_none=True, mode='json'))
         new_attachment = Attachment(id=response['data']['id'], **response['data']['attributes'])
         self._internal_post_with_files(endpoint=f'{new_attachment.id}/upload',
-                                       data=AttachmentsApi.__bypass_firefly_mime_detection(data))
+                                       data=data)
 
         return new_attachment
-
-    @staticmethod
-    def __is_csv(data: bytes) -> bool:
-        try:
-            csv.Sniffer().sniff(str(data))
-            return True
-        except csv.Error:
-            return False
-
-    @staticmethod
-    def __bypass_firefly_mime_detection(data: bytes) -> bytes:
-        if not AttachmentsApi.__is_csv(data):
-            return data
-        return b'\n' + data
